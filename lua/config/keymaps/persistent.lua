215d902cf34c41c2f88be5f6965f639ab8159d23
mitsuhiro-hatano <hatamonroid@gmail.com>
2026-02-20 08:57:06 +0900

    toggleterm

--- lua/config/keymaps/persistent.lua
+++ lua/config/keymaps/persistent.lua
@@ -9,3 +9,12 @@ keymap.set("n", "H", ":BufferLineCyclePrev<CR>", { silent = true, desc = "Prev B
 
 -- 今後、例えばステータスライン関連や、
 -- 常に有効にしたいプラグインの操作が出てきたらここに追記していく！
+
+-- toggleterm
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<C-]>', [[<C-\><C-n>]], opts)
+end
+
+-- ターミナルが開いた時にだけこの設定を適用する
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
