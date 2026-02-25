44ed35e34dbd1e4c8d47e6fa67642b536644a6d2
mitsuhiro-hatano <hatamonroid@gmail.com>
2026-02-26 07:46:33 +0900

    move

--- lua/plugins/nvim/move.lua
+++ lua/plugins/nvim/move.lua
@@ -4,7 +4,7 @@ return {
   opts = {}, -- 基本設定だけでも十分最強！
   keys = {
     {
-      "s",
+      "<leader>s",
       mode = { "n", "x", "o" },
       function()
         require("flash").jump()
@@ -12,7 +12,7 @@ return {
       desc = "Flash",
     },
     {
-      "S",
+      "<leader>S",
       mode = { "n", "x", "o" },
       function()
         require("flash").treesitter()
