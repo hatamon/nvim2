7cedd305e92640e4d742efa03debf6884899eb1d
mitsuhiro-hatano <hatamonroid@gmail.com>
2026-02-25 16:28:27 +0900

    move

---
+++ lua/plugins/nvim/move.lua
@@ -0,0 +1,23 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {}, -- 基本設定だけでも十分最強！
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+  },
+}
